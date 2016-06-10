e047eb085a597b09060915c5f4849e0011c3a6dc
Pedro Gutiérrez <pgug06@gmail.com>
2016-06-10 10:31:12 -0600

    Set up

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mkboxV01', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
